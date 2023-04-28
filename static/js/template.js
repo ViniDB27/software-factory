4e06b5c913db65b36c551237488a45a2c3eb3eb0
Vinicio D. Brejinski <viniciosdb@gmail.com>
2023-04-28 09:04:17 -0300

    menu mobile add

--- static/js/template.js
+++ static/js/template.js
@@ -1,12 +1,18 @@
 let menuOpen = false;
 
 const menu = document.querySelector('.mobile-nav-wrapper');
+const submenu = document.querySelector('.nav-bar-mobile-items-container');
 
 const menuButton = document.querySelector('.menu-btn');
 
-menuButton.addEventListener('CLICK', (element) => {
-    console.log(element)
+menuButton.addEventListener('click', (element) => {
+    if (!menuOpen) {
+        menu.classList.add('mobile-nav-wrapper-open');
+        submenu.style.display = 'flex';
+        menuOpen = true;
+    } else {
+        menu.classList.remove('mobile-nav-wrapper-open');
+        submenu.style.display = 'none'
+        menuOpen = false;
+    }
 })
-
-console.log(menu)
-
