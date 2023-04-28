1a1b596a4e4b47539600b6e604f218e2c49ada3c
vinicio Brejinski <viniciosdb@gmail.com>
2023-04-28 08:24:50 -0300

    menu mobile

---
+++ static/js/template.js
@@ -0,0 +1,12 @@
+let menuOpen = false;
+
+const menu = document.querySelector('.mobile-nav-wrapper');
+
+const menuButton = document.querySelector('.menu-btn');
+
+menuButton.addEventListener('CLICK', (element) => {
+    console.log(element)
+})
+
+console.log(menu)
+
